189bb5364afb3b93814b71ef7673a510ce29b9de
origin <git@github.com:Azaharov14/helloworld.git>
2024-06-09 23:44:05 +0300

    2 commit

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,29 @@
+import java.sql.SQLOutput;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
+        System.out.println("Task1");
+
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = "Ivanov " + "Ivan " + "Ivanovich ";
+        System.out.println("Ф.И.О сотрудника - " + fullName);
+
+        System.out.println("Task2");
+
+        String fullNameS = "Ivanov Ivan Ivanovich";
+        String fullNamesTrue = fullNameS.replace("Ivanov Ivan Ivanovich",
+                "IVANOV IVAN IVANOVICH");
+        System.out.println(fullNamesTrue);
+
+        System.out.println("Task3");
+
+        String fullNameOne = "Иванов Семён Семёнович";
+        String fullNameOneTrue = fullNameOne.replace("ё","е");;
+        System.out.println("Данные Ф.И.О. сотрудника-" + fullNameOne);
     }
 }
