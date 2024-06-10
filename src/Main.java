3f1490936ccc54b909c1051d06f47f353ca7b2b6
origin <git@github.com:Azaharov14/helloworld.git>
2024-06-10 08:59:40 +0300

    3 commit

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,9 @@
-import java.sql.SQLOutput;
-
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Task1");
+        System.out.println("Задача 1");
 
         String firstName = "Ivan";
         String middleName = "Ivanovich";
@@ -13,17 +11,17 @@ public class Main {
         String fullName = "Ivanov " + "Ivan " + "Ivanovich ";
         System.out.println("Ф.И.О сотрудника - " + fullName);
 
-        System.out.println("Task2");
+        System.out.println("Задача 2");
 
-        String fullNameS = "Ivanov Ivan Ivanovich";
+        String fullNameS = ("Ivanov Ivan Ivanovich");
         String fullNamesTrue = fullNameS.replace("Ivanov Ivan Ivanovich",
                 "IVANOV IVAN IVANOVICH");
         System.out.println(fullNamesTrue);
 
-        System.out.println("Task3");
+        System.out.println("Задача 3");
 
         String fullNameOne = "Иванов Семён Семёнович";
-        String fullNameOneTrue = fullNameOne.replace("ё","е");;
-        System.out.println("Данные Ф.И.О. сотрудника-" + fullNameOne);
+        String fullNameOneTrue = fullNameOne.replace("ё","е");
+        System.out.println("Данные Ф.И.О. сотрудника-" + fullNameOneTrue);
     }
 }
